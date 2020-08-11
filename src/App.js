423f9554c430aa8ee44b8516a2758cb491fb23b6
unknown <kw2577@naver.com>
2020-08-11 20:59:50 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
+
 class App extends React.Component{
   state = {
     isLoading: true,
@@ -12,7 +14,7 @@ class App extends React.Component{
         data : { movies }
       }
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading: false });
+    this.setState({movies, isLoading:false});
   }
   componentDidMount(){
     this.getMovies();
--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }){
     return (
-        <div>
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movies">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">
-                    {genres.map(genre => (
-                        <li className="genres__genre">{genre}</li>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">{genre}</li>
                     ))}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 140)}...</p>
             </div>
         </div>
     );
